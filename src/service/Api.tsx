1ac463841c8d691a712a43c6d14d5c84d7d6c7ee
ervinjusufagic <ervinjusufagic@gmail.com>
2022-01-04 23:03:23 +0100

    Android fix

--- src/service/Api.tsx
+++ src/service/Api.tsx
@@ -1,7 +1,17 @@
-export const API_BASE_URL = "http://localhost:3000";
+import { Platform } from "react-native";
+
+export const API_BASE_URL =
+  Platform.OS === "ios" ? "http://localhost:3000" : "http://10.0.2.2:3000";
 
 // Queries
 export const fetchAllCats = async () => {
-  const response = await (await fetch(`${API_BASE_URL}/cats`)).json();
+  const response = await (
+    await fetch(`${API_BASE_URL}/cats`, {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json", // I added this line
+      },
+    })
+  ).json();
   return response;
 };
