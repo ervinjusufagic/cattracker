ae5ed3ff352f63eafa1eab2dda831838d4ebf3da
ervinjusufagic <ervinjusufagic@gmail.com>
2022-01-08 15:59:59 +0100

    scroll to selected

--- src/Components/ImageCarousel.tsx
+++ src/Components/ImageCarousel.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import {
   Image,
   ScrollView,
@@ -15,8 +15,41 @@ const ImageCarousel = ({
   onSelectImage: (image: string) => void;
   selectedImage: string;
 }) => {
+  let scrollViewRef: ScrollView | null = null;
+
+  useLayoutEffect(() => {
+    setTimeout(() => {
+      scrollViewRef?.scrollTo({ x: calculateScroll(), animated: true });
+    }, 1);
+    // Need to delay for ios
+  });
+
+  const getSelectedImageIndex = () => {
+    let imageIndex: number = 0;
+
+    Object.keys(imageMap).map((key, i) => {
+      if (key === selectedImage) {
+        imageIndex = i;
+      }
+    });
+
+    return imageIndex;
+  };
+
+  const calculateScroll = () => {
+    const imageIndex = getSelectedImageIndex();
+
+    const imageXPosition = 200 * imageIndex;
+    const margin = imageIndex * 10;
+
+    return imageXPosition + margin;
+  };
+
   return (
-    <ScrollView horizontal>
+    <ScrollView
+      pagingEnabled={true}
+      ref={ref => (scrollViewRef = ref)}
+      horizontal>
       <View style={styles.scrollViewContent}>
         {Object.keys(imageMap).map((key, i) => (
           <TouchableOpacity
